8cf3ab903766c0fbc9b686b6ed74bbf354aad39f
Ilia <izagorodn@gmail.com>
2021-01-19 17:45:47 +0300

    changed greeting and removed .idea folder from git

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({hi: 'my name is Ilia!'});
+	res.send({hi: 'my name is Ilia! my app is here!'});
 });
 
 const PORT = process.env.PORT || 5000;
