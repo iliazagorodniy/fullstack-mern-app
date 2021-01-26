7384527f6bf2f98650b7b2819d3a3d0d09e1e833
Ilia <izagorodn@gmail.com>
2021-01-26 15:00:09 +0300

    fixed http/https heroku issue with adding proxy: true to google strategy

--- services/passport.js
+++ services/passport.js
@@ -50,7 +50,8 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
+			callbackURL: '/auth/google/callback',
+			proxy: true
 		},
 		(accessToken, refreshToken, profile, done) => { // комментарий *7
 			console.log('profile nigga: ', profile);
