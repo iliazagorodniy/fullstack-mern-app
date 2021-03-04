b210b41daeea53c6d9397bc2b0bb038415f287a3
Ilia <izagorodn@gmail.com>
2021-03-04 19:16:20 +0300

    react router and redux setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,19 @@
 // Rendering layer control (React Router)
 
 import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 function App(props) {
 	return (
 		<div>
-			Hello there!
+			<BrowserRouter>
+				<Route path="/" component={Landing}></Route>
+			</BrowserRouter>
 		</div>
 	);
 }
