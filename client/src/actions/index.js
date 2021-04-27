77153aacb4ede03126c0520e5ce36f062c468fec
Ilia <izagorodn@gmail.com>
2021-04-28 02:19:25 +0300

    refactor app, add dispatch from redux-thunk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,5 +2,9 @@ import axios from "axios";
 import { FETCH_USER } from "./types";
 
 const fetchUser = () => {
-	axios.get('/api/current_user')
+	return function(dispatch) {
+		axios
+			.get('/api/current_user')
+			.then(response => dispatch({ type: FETCH_USER, payload: response }))
+	}
 }
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 // Rendering layer control (React Router)
 
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
 
@@ -8,17 +8,27 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-function App(props) {
-	return (
-		<div className="container">
-			<BrowserRouter>
-				<Header />
-				<Route exact path="/" component={Landing}></Route>
-				<Route exact path="/surveys" component={Dashboard}></Route>
-				<Route path="/surveys/new" component={SurveyNew}></Route>
-			</BrowserRouter>
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<Header />
+					<Route exact path="/" component={Landing}></Route>
+					<Route exact path="/surveys" component={Dashboard}></Route>
+					<Route path="/surveys/new" component={SurveyNew}></Route>
+				</BrowserRouter>
+			</div>
+		);
+	}
 }
 
 export default App;
