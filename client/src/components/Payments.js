e151bcf33045953e13596755d96438a48e2d9f48
Ilia <izagorodn@gmail.com>
2021-06-01 20:57:28 +0300

    106 Reusing action types

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -6,10 +6,17 @@ class Payments extends Component {
 		return (
 			<div>
 				<StripeCheckout
+					allowRememberMe={true}
+					name="Emaily"
+					description="$5 for 5 email credits"
 					amount={500}
 					token={token => console.log(token)}
 					stripeKey={process.env.REACT_APP_STRIPE_KEY}
-				/>
+				>
+					<button className={"btn"}>
+						Add Credits
+					</button>
+				</StripeCheckout>
 			</div>
 		);
 	}
