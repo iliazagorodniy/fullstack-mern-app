d51871d9589ea4fda98842911fa3e54f33207581
Ilia <izagorodn@gmail.com>
2021-06-07 18:55:25 +0300

    added body parser middleware, adding a charge object is in process

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser')
 
 require('./models/User');
 require('./services/passport');
@@ -36,6 +37,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json())
 app.use(
 	cookieSession({ // cookie config object
 		maxAge: 30 * 24 * 60 * 60 * 1000, // how long cookie gonna live before it expires
@@ -47,6 +49,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/BillingRoutes')(app)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
